6801bb4bec9c2ee5c3a2d852c1d4747cd1c2fae8
Gearonix <egor.uzhanin@gmail.com>
2023-07-04 18:25:59 +0300

    fix: ErrorBoundary

--- libs/client-shared/src/lib/components/ErrorBoundary/ErrorBoundary.tsx
+++ libs/client-shared/src/lib/components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,16 +1,9 @@
-import { Component, Fragment } from 'preact'
 import { useErrorBoundary } from 'preact/hooks'
-import { useRouteError } from 'react-router-dom'
+
+import { WithChildren } from '../../../types'
 
 import ErrorTemplate from './ui/ErrorTemplate'
 
-import { Nullable,WithChildren } from '$/shared'
-
-
-interface State{
-  hasError: boolean,
-  errorInfo: Nullable<string>
-}
 const ErrorBoundary = ({ children }: WithChildren) => {
   const [error] = useErrorBoundary()
 
@@ -24,29 +17,5 @@ const ErrorBoundary = ({ children }: WithChildren) => {
     {children}
   </>
 }
-
-
-// class ErrorBoundary extends Component<WithChildren, State> {
-//   public state: State = {
-//     hasError: false,
-//     errorInfo: null
-//   }
-//   public static getDerivedStateFromError(error: Error): State {
-//     return { hasError: true, errorInfo: error.message }
-//   }
-//
-//   public componentDidCatch(error: Error) {
-//     console.error('Uncaught fatal error:', error)
-//   }
-//
-//   render() {
-//     if (this.state.hasError) {
-//       return <ErrorTemplate errorInfo={this.state.errorInfo as string}/>
-//     }
-//
-//     return this.props.children
-//   }
-// }
-
 
 export default ErrorBoundary
