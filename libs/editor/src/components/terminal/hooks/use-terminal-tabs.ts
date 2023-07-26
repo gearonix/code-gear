d16c24134ea394389b2fe79dab57365a449ddd0b
Gearonix <egor.uzhanin@gmail.com>
2023-07-26 21:38:05 +0300

    [trying to solve git bugs]

---
+++ libs/editor/src/components/terminal/hooks/use-terminal-tabs.ts
@@ -0,0 +1,32 @@
+import { useCallback } from 'react'
+
+import { useModalsContext } from '@/shared/hooks'
+
+export type TerminalTabKeys = 'terminal' | 'test_cases'
+
+interface TerminalTab {
+  label: string
+  key: TerminalTabKeys
+}
+
+const terminalTabs: TerminalTab[] = [
+  { label: 'Terminal', key: 'terminal' },
+  { label: 'Test cases', key: 'test_cases' }
+]
+
+export const useTerminalTabs = () => {
+  const modalsContext = useModalsContext()
+  const activeKey = modalsContext.state.selectedTerminalTab
+
+  const setActiveKey = useCallback((key: TerminalTabKeys) => {
+    modalsContext.update({
+      selectedTerminalTab: key
+    })
+  }, [])
+
+  return {
+    key: activeKey,
+    set: setActiveKey,
+    val: terminalTabs
+  }
+}
