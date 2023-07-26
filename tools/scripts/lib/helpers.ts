836d4d35a000199949ffa080f7234b13e3971003
Gearonix <egor.uzhanin@gmail.com>
2023-07-26 22:19:55 +0300

    fix: trying to solve git bugs with caching old files

--- tools/scripts/lib/helpers.ts
+++ tools/scripts/lib/helpers.ts
@@ -1,17 +1,17 @@
 import { join } from 'path'
 import { Project, SourceFile } from 'ts-morph'
 
-const resolveRoot = () => join(__dirname, '..', '..')
+const resolveRoot = () => join(__dirname, '..', '..', '..')
 
 const runProjectFiles = async (cb: (file: SourceFile) => void) => {
   const project = new Project({
     tsConfigFilePath: join(resolveRoot(), 'tsconfig.base.json')
   })
 
-  project.addSourceFilesAtPaths('../../libs/**/*.ts')
-  project.addSourceFilesAtPaths('../../libs/**/*.tsx')
-  project.addSourceFilesAtPaths('../../apps/**/*.ts')
-  project.addSourceFilesAtPaths('../../apps/**/*.tsx')
+  project.addSourceFilesAtPaths('../../../libs/**/*.ts')
+  project.addSourceFilesAtPaths('../../../libs/**/*.tsx')
+  project.addSourceFilesAtPaths('../../../apps/**/*.ts')
+  project.addSourceFilesAtPaths('../../../apps/**/*.tsx')
 
   const files = project.getSourceFiles()
 
