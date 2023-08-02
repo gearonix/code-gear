d61d2c0d9a5323d41c54940724f6861a40a923ff
Gearonix <egor.uzhanin@gmail.com>
2023-08-02 13:24:08 +0300

    chore: configured commitlint, moved from pnpm to yarn 3 with plugins

---
+++ commitlint.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case']],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'refactor',
+        'style',
+        'test',
+      ]
+    ]
+  }
+};
