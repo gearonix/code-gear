09802d4e8d61885deacf854b3b59011e037cc7a0
Gearonix <egor.uzhanin@gmail.com>
2023-10-10 21:50:53 +0300

    feat(vitepress): initialized docs

---
+++ apps/server/service-auth/src/main.ts
@@ -0,0 +1,32 @@
+import { MicroserviceOptions } from '@nestjs/microservices'
+import { NestFactory } from '@nestjs/core'
+import { KafkaService } from '@code-gear/api/common'
+import { ListenerService } from '@code-gear/api/common'
+import { Microservice } from '@code-gear/api/common'
+import { RpcExceptionFilter } from '@code-gear/api/common'
+import { ValidationPipe } from '@code-gear/api/common'
+import { TemplateModule } from './auth.module'
+
+const bootstrap = async () => {
+  const app = await NestFactory.create(TemplateModule)
+
+  const kafkaService = app.get(KafkaService)
+
+  app.connectMicroservice<MicroserviceOptions>(
+    kafkaService.getKafkaOptions(Microservice.TEMPLATE),
+  )
+
+  app.useGlobalFilters(new RpcExceptionFilter())
+
+  app.useGlobalPipes(new ValidationPipe())
+
+  await app.startAllMicroservices()
+
+  await app.init()
+
+  const listener = app.get(ListenerService)
+
+  await app.listen(listener.PORT, listener.getListenerCallback())
+}
+
+bootstrap()
